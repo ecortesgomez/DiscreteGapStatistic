567b5bdaccda764b448f023328a00e8457d4145b
Cortes Gomez <ED33454@roswellpark.org>
2023-11-27 16:12:35 -0500

    Second commit

--- R/DiscreteClusGapFuns.R
+++ R/DiscreteClusGapFuns.R
@@ -4,11 +4,15 @@
 #' @param FUNcluster a function that accepts as first argument a matrix like `x`; second argument specifies number of `k` (k=>2) clusters
 #' This functions returns a list with a component named `cluster`, a vector of length `n=nrow(x)` of integers from `1:k` indicating observation cluster assignment.
 #' @param K.max Maximum number of clusters `k` to consider
-#' @param vals String or numerical vector. A vector with all categories to consider when bootstrapping the null distribution sample (FR: Full Range option).
+#' @param value.range String, character vector or a list of character vector with the length matching the number of columns (nQ) of the array.
+#' A vector with all categories to consider when bootstrapping the null distribution sample (FR: Full Range option).
 #' By DEFAULT vals=NULL, meaning unique range of categories found in the data will be used when drawing the null (DR: Data Range option).
+#' If a character vector of categories is provided, these values would be used for the null distribution drawing across the array.
+#' If a list with category character vectors is provided, it has to have the same number of columns as the input array. The order of list element corresponds to the array's columns.
 #' @param integer or logical, determining if “progress” output should be printed. The default prints one bit per bootstrap sample.
-#' @param distName Name of categorical distance to apply
-#' @param B Number of bootstrap samples.
+#' @param distName Name of categorical distance to apply.
+#' Available distances: 'bhattacharyya', 'chisquare', 'cramerV', 'hamming', 'hellinger',
+#' @param B Number of bootstrap samples. By default B = nrow(x).
 #' @param verbose integer or logical determining whether progress output should printed while running. By DEFAULT one bit is printed per bootstrap sample.
 #' @param ... optionally further arguments for `FUNcluster()`
 #'
@@ -18,8 +22,8 @@
 clusGapUDiscr <- function (x,
                            FUNcluster,
                            K.max,
-                           B = 100,
-                           vals=NULL,
+                           B = nrow(x),
+                           value.range = 'DR',
                            verbose = interactive(),
                            distName = 'hamming', ...
                            ## seed = 17,
@@ -34,6 +38,20 @@ clusGapUDiscr <- function (x,
               K.max >= 2,
               (n <- nrow(x)) >= 1,
               ncol(x) >= 1)
+
+    ## Reformatting if the array is found to be numerical
+   uniLevs <- unique(as.vector(x))
+   if(is.numeric(x)){
+       message(paste0('x array is numerical and has ', length(uniLevs), ' levels.'))
+       stopifnot(length(uniLevs) < 10)
+       message('Array values would be transformed to categorical levels: ')
+       myCats <- paste0(paste0(uniLevs, ' -> c', uniLevs), collapse = ', ')
+       message(myCats)
+    }else{
+       message(paste0('Found levels: ',
+                      paste0(uniLevs,
+                             collapse = ', ')) )
+    }
 
     if(B != (B. <- as.integer(B)) || (B <- B.) <= 0)
          stop("'B' has to be a positive integer")
@@ -51,7 +69,8 @@ clusGapUDiscr <- function (x,
         ## This is where the distance functions comes into place!
 
         clus <- if(kk > 1){
-                    FUNcluster(X, kk)$cluster
+                    Xdist <- distancematrix(X, d = distName)
+                    FUNcluster(Xdist, kk)$cluster
                 }else{
                     rep.int(1L, nrow(X))
                 }
@@ -63,11 +82,10 @@ clusGapUDiscr <- function (x,
         mySplit <- split(ii, clus)
         mySplit <- mySplit[sapply(mySplit, length) != 1]
 
-        ## 0.5* sum(vapply(split(ii, clus),
         0.5* sum(vapply(mySplit,
                         function(I) {
                             xs <- X[I,, drop=FALSE]
-                            sum(distancematrix(xs, d=distName) / nrow(xs),
+                            sum(distancematrix(xs, d = distName) / nrow(xs),
                                 na.rm=TRUE)
                         }, 0.))
     }
@@ -81,7 +99,21 @@ clusGapUDiscr <- function (x,
     if(verbose) cat("done\n")
 
     ## Column ranges
-    rng.x1 <- apply(x, 2L, range)
+    if(is.character(value.range) & !is.list(value.range) & value.range[1] == 'DR' &
+       length(value.range) == 1){
+         vals <- NULL
+         ## rng.x1 <- apply(x, 2L, unique)
+         rng.x1 <- lapply(1:ncol(x), function(i) unique(x[, i]))
+
+    }else if(all(is.character(value.range) ) &
+             !is.list(value.range) &
+             (length(value.range) > 1) ){
+         vals <- value.range
+
+    }else if(!is.character(value.range) & is.list(value.range)){
+         stopifnot(ncol(x) == length(value.range))
+         vals <- value.range
+    }
 
     ## 2. Bootstrap Piece
     logWks <- matrix(0, B, K.max)
@@ -93,19 +125,26 @@ clusGapUDiscr <- function (x,
         ## Generate "H0"-data as "parametric bootstrap sample" :
 
         if(is.null(vals)){
-            ## Here we're doing the discrete uniform resampling across column ranges.
-            z <- apply(rng.x1, 2,
+            ## DR: Data Range option
+            z <- lapply(rng.x1,
                        function(M, nn) sample(size = nn,
-                                              x = seq(M[1], M[2]),
+                                              x = M,
                                               replace = TRUE),
-                       nn=n)
-        }else{
-            ## The null samples over all possible discrete values.
-            z <- apply(rng.x1, 2,
-                       function(M, nn) sample(size = nn,
-                                              x = vals, ## Discrete values to use
-                                              replace = TRUE),
-                       nn=n)
+                       nn=n) %>%
+               do.call(what = cbind)
+
+        }else if(!is.list(vals) & length(vals) > 2 ){
+            ## Single vector with all levels
+            z <- matrix(sample(x = vals, size = nrow(x)*ncol(x), replace=TRUE),
+                        nrow = nrow(x))
+
+        }else if(is.list(vals)){
+           z <- sapply(1:length(vals),
+                      function(i, nn) sample(size = nn,
+                                             x = vals[[i]], ## Discrete values to use
+                                             replace = TRUE),
+                      nn=n)
+           ##
         }
 
         for(k in 1:K.max) {
