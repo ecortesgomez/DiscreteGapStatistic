b7330d8283ec490eb057e7d23a716946136b71cd
Cortes Gomez <ED33454@roswellpark.org>
2025-05-23 00:14:45 -0400

    Updated SimData. Now can produce clearly defined ordinal data.

--- R/SimDataFun.R
+++ R/SimDataFun.R
@@ -5,8 +5,14 @@
 #'
 #' @param N Integer. Number of observations.
 #' @param nQ Integer. Number of questions.
-#' @param pi Numeric vector. Vector of probabilities adding up to 1; it is recommended that names of elements are character strings.
-#' Alternatively, pi can be list of vectors as previously described with length equal to `nQ`. Notice that the list elements need not have same vector names. The order of pi vectors in the list will be reflected in the resulting simulated matrix. This alternative ideally assumes that questions are independently distributed.
+#' @param pi Numeric vector. Vector of probabilities adding up to 1.
+#' Alternatively, pi can be list of vectors as previously described with length equal to `nQ`.
+#' This case, notice that that the vectors within the list can be different.
+#' The order of the pi vectors in the list will be reflected in the resulting column names.
+#' If `dataClass = 'ordinal'` it is required that vector names of `pi` be integers and decimals or other numeric values should be avoided.
+#' If `dataClass = nominal` and pi vector names are numerical, these will remain characters.
+#' @param dataClass Character. Either 'nominal' or 'ordinal'.
+#' @param seed Integer. Numerical seed for the RNG.
 #'
 #' @returns N x nQ matrix with simulated categories distributed according to vector pi
 #' @export
@@ -16,36 +22,89 @@
 #' X <- SimData(N=10, nQ=5, Pix)
 #' head(X)
 #'
-#' Piy <- setNames(c(0.3, 0.2, 0.4, 0, 0.1), paste0('a', 1:5))
+#' Piy <- setNames(c(0.3, 0.2, 0.5), paste0('b', 1:3))
 #' Y <- SimData(N=10, nQ=3, Piy)
 #' head(Y)
 #'
-#' PiZ <- list(x1 = Pix, x2 = Pix, y1 = Piy, y2 = Piy)
-#' Z <- SimData(N=10, nQ=length(PiZ), PiZ)
+#' PiZ <- list(x1 = Pix, y1 = Piy, y2 = Piy)
+#' Z <- SimData(N = 10, nQ = length(PiZ), PiZ)
 #'
-SimData <- function(N, nQ, pi){
+#' Piw <- setNames(Piy, 1:3)
+#' W <- SimData(N=10, nQ=3, Piw, dataClass = 'ord')
+#' head(W)
 
-   ## if(class(pi) == 'numeric'){
-   if(is.numeric(pi)){
-      out <- matrix(sample(names(pi), N*nQ, replace=TRUE, prob = pi),
-                    nrow = N,
-                    dimnames = list(paste0('s', 1:N),
-                                    paste0('q', 1:nQ)))
-      return(out)
-   ## }else if(class(pi) == 'list' & length(pi) == nQ){
-   }else if(is.list(pi) & length(pi) == nQ){
-      out <-lapply(pi, function(y) sample(x = names(y), size = N, replace=TRUE, prob = y)) %>%
-         do.call(what = cbind)
-      rownames(out) <- paste0('s', 1:N)
+SimData <- function(N, nQ, pi,
+                    dataClass = 'nominal',
+                    seed = NULL){
 
-      if(all( class(names(pi)) == 'character' ) )
-         colnames(out) <- names(pi)
-      else
-         colnames(out) <- paste0('1', 1:nQ)
+   if(!is.null(seed))
+      set.seed(seed)
 
-      return(out)
-   }else{
-      message('Either pi is numeric vector or a list with numeric vectors of length equal to nQ.')
+   if(dataClass == 'nominal' | grepl('^nom', dataClass, ignore.case=TRUE)){
+
+      if(is.numeric(pi)){ ## Just a numerical vector
+         stopifnot(sum(pi) == 1 )
+         out <- matrix(sample(names(pi), N*nQ, replace=TRUE, prob = pi),
+                       nrow = N,
+                       dimnames = list(paste0('s', 1:N),
+                                       paste0('q', 1:nQ)))
+         return(out)
+
+      }else if(is.list(pi) & length(pi) == nQ){
+
+         stopifnot(all(lapply(pi, sum) == 1))
+         out <-lapply(pi,
+                      function(y) sample(x = names(y),
+                                         size = N,
+                                         replace = TRUE,
+                                         prob = y)) %>%
+            do.call(what = cbind)
+         rownames(out) <- paste0('s', 1:N)
+
+         if(all( class(names(pi)) == 'character' ) )
+            colnames(out) <- names(pi)
+         else
+            colnames(out) <- paste0('Q', 1:nQ)
+
+         return(out)
+      }else{
+         message(paste0('Either pi is numeric vector with no names ',
+                        'or pi is a list with numeric vectors of length no equal to nQ.',
+                        'If pi is a list, make sure that length(pi) == nQ'))
+      }
+   }else if(dataClass == 'ordinal' | grepl('^ord', dataClass, ignore.case=TRUE)){
+
+      if(is.numeric(pi) ){ ## Just a vector
+         stopifnot(sum(pi) == 1)
+         out <- matrix(sample(names(pi) %>% as.integer,
+                              N*nQ,
+                              replace=TRUE,
+                              prob = pi),
+                       nrow = N,
+                       dimnames = list(paste0('s', 1:N),
+                                       paste0('q', 1:nQ)))
+         return(out)
+
+      }else if(is.list(pi) & length(pi) == nQ){
+         stopifnot(all(lapply(pi, sum) == 1))
+         out <-lapply(pi,
+                      function(y) sample(x = names(y) %>% as.integer,
+                                         size = N,
+                                         replace = TRUE,
+                                         prob = y)) %>%
+            do.call(what = cbind)
+         rownames(out) <- paste0('s', 1:N)
+
+         if(all( class(names(pi)) == 'character' ) )
+            colnames(out) <- names(pi)
+         else
+            colnames(out) <- paste0('Q', 1:nQ)
+
+         return(out)
+      }else{
+         message(paste0('Either pi is numeric vector with no names ',
+                        'or pi is a list with numeric vectors of length no equal to nQ.',
+                        'If pi is a list, make sure that length(pi) == nQ'))
+      }
    }
-
 }
