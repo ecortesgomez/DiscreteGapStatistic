924696f7e345ddc0ea0ce89277536fc8b35b0c50
Cortes Gomez <ED33454@roswellpark.org>
2026-02-07 12:20:28 -0500

    Simplify findK function

--- R/DiscreteClusGapFuns.R
+++ R/DiscreteClusGapFuns.R
@@ -409,7 +409,7 @@ clusGapDiscr <- function(x,
 
 #' Criteria to determine number of clusters k
 #'
-#' Same function as found in `cluster` package.
+#' Similar to `maxSE` function found the `cluster` package.
 #'
 #' @import dplyr
 #' @param cG_obj Output object obtained from `clusGapDiscr`
@@ -420,34 +420,17 @@ clusGapDiscr <- function(x,
 
 findK <- function (cG_obj, meth = "Tibs2001SEmax", SE.fact = 1){
    logW <- gap <- SE.sim <- NULL
-   if (!meth %in% c("minSE", "minGap", "maxChange")) {
 
-      ## Tibs2001 SEmax criterion
-      # myTab <- data.frame(cG_obj$Tab) %>%
-      #    mutate(nClus = 1:nrow(cG_obj$Tab) ) %>%
-      #    subset(is.finite(logW) & !is.na(gap) & !is.na(SE.sim))
+   outDF <- data.frame(cG_obj['Tab'])
+   outDF[, 'nClus'] <- 1:nrow(outDF)
+   colnames(outDF) <- c('logW', 'E.logW', 'gap', 'SE.sim', 'nClus')
 
-      outDF <- data.frame(cG_obj['Tab'])
-      outDF[, 'nClus'] <- 1:nrow(outDF)
-      colnames(outDF) <- c('logW', 'E.logW', 'gap', 'SE.sim', 'nClus')
-
-      myTab <- outDF %>%
-         subset(is.finite(logW) & !is.na(gap) & !is.na(SE.sim))
-      selK <- with(myTab,
-                   cluster::maxSE(f = gap,
-                                  SE.f = SE.sim,
-                                  method = meth,
-                                  SE.factor = SE.fact) )
-      myTab[selK, 'nClus']
-
-   }
-   else if (meth == "minSE") {
-      which.min(cG_obj$Tab[, "SE.sim"])
-   }
-   else if (meth == "minGap") {
-      which.min(cG_obj$Tab[, "gap"])
-   }
-   else if (meth == "maxChange") {
-      which.max(abs(cG_obj$Tab[, "gap"])) + 1
-   }
+   myTab <- outDF %>%
+      subset(is.finite(logW) & !is.na(gap) & !is.na(SE.sim))
+   selK <- with(myTab,
+                cluster::maxSE(f = gap,
+                               SE.f = SE.sim,
+                               method = meth,
+                               SE.factor = SE.fact) )
+   myTab[selK, 'nClus']
 }
